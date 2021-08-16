0271167ae24881c925a7b2419007ed1893a7469c
Mohammad Aghamir <aghamir.m@apa.ce.sharif.ir>
2021-08-16 15:54:37 +0430

    Refactor: rename package to avoid conflict with original one

--- src/lib.rs
+++ src/lib.rs
@@ -636,7 +636,7 @@ impl<'a> FromPyObject<'a> for ExtendedKeypair {
 
 /// This module is a python module implemented in Rust.
 #[pymodule]
-fn sr25519(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+fn sr25519_nbx(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(pair_from_seed))?;
     m.add_wrapped(wrap_pyfunction!(pair_from_ed25519_secret_key))?;
     m.add_wrapped(wrap_pyfunction!(sign))?;
