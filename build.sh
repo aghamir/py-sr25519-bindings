56d64b86ee08c0c304e8dfac90bfb0266f415752
Hasanizadeh <parizad1188@gmail.com>
2021-08-16 11:29:56 +0430

    add multi-sign helper functions.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 maturin build
 
 # Build manylinux1 wheelhouse
-docker build . --tag polkasource/maturin
-docker run --rm -i -v $(pwd):/io polkasource/maturin build
+sudo docker build . --tag polkasource/maturin
+sudo docker run --rm -i -v $(pwd):/io polkasource/maturin build
